ec916936c8ac35460acbb909e9b74d39654f7b5c
Sergey O <so@fas.harvard.edu>
2022-02-22 15:43:17 -0500

    Update design.py
    increasing default number of hard_iters to 50

--- af/design.py
+++ af/design.py
@@ -537,13 +537,13 @@ class mk_design_model:
     ''' optimize argmax(logits)'''
     self.design(iters, soft=True, hard=True, **kwargs)
 
-  def design_2stage(self, soft_iters=100, temp_iters=100, hard_iters=10, **kwargs):
+  def design_2stage(self, soft_iters=100, temp_iters=100, hard_iters=50, **kwargs):
     '''two stage design (soft→hard)'''
     self.design(soft_iters, soft=True, temp=1.0, **kwargs)
     self.design(temp_iters, soft=True, temp=1.0,  e_temp=1e-2, **kwargs)
     self.design(hard_iters, soft=True, temp=1e-2, hard=True, save_best=True, **kwargs)
 
-  def design_3stage(self, soft_iters=300, temp_iters=100, hard_iters=10, **kwargs):
+  def design_3stage(self, soft_iters=300, temp_iters=100, hard_iters=50, **kwargs):
     '''three stage design (logits→soft→hard)'''
     self.design(soft_iters, soft=False, e_soft=True, **kwargs)
     self.design(temp_iters, soft=True, temp=0.5, e_temp=1e-2, **kwargs)

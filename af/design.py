92a54bd65a009186b5e204d5b3ac050464917abf
Sergey O <so@fas.harvard.edu>
2022-04-14 09:38:55 -0400

    fixing nan issues

--- af/design.py
+++ af/design.py
@@ -199,7 +199,7 @@ class mk_design_model:
       else:
         con_bins = dgram_bins < dgram_bins[-1]
       con_prob = jax.nn.softmax(outputs["distogram"]["logits"])
-      con_loss = -jnp.log((con_bins*con_prob).sum(-1))
+      con_loss = -jnp.log((con_bins*con_prob).sum(-1) + 1e-8)
 
       # protocol specific losses
       if self.protocol == "binder":

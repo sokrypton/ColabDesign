04afd3590c2c4c1e78bb1aecbb33f5191755b94b
Sergey O <so@fas.harvard.edu>
2022-05-08 19:01:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 setup(
     name='ColabDesign',
     version='0.0.0',
-    packages=find_packages(include=['af'])
+    packages=find_packages(include=['../ColabDesign'])
 )

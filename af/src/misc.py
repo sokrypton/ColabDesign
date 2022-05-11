643bbda7cdba4e4d855163a58dab7bd8a546fac4
Sergey O <so@fas.harvard.edu>
2022-05-11 14:43:06 -0400

    remove unused functions

--- af/src/misc.py
+++ af/src/misc.py
@@ -4,45 +4,8 @@ import tensorflow as tf
 tf.config.set_visible_devices([], 'GPU')
 
 import numpy as np
-from alphafold.common import protein
-from alphafold.common import residue_constants
-from alphafold.model import model
-from alphafold.model import folding
-from alphafold.model import all_atom
-from alphafold.model.tf import shape_placeholders
-
-#######################
-# reshape inputs
-#######################
-def make_fixed_size(feat, model_runner, length, batch_axis=True):
-  '''pad input features'''
-  cfg = model_runner.config
-  if batch_axis:
-    shape_schema = {k:[None]+v for k,v in dict(cfg.data.eval.feat).items()}
-  else:
-    shape_schema = {k:v for k,v in dict(cfg.data.eval.feat).items()}
-
-  pad_size_map = {
-      shape_placeholders.NUM_RES: length,
-      shape_placeholders.NUM_MSA_SEQ: cfg.data.eval.max_msa_clusters,
-      shape_placeholders.NUM_EXTRA_SEQ: cfg.data.common.max_extra_msa,
-      shape_placeholders.NUM_TEMPLATES: cfg.data.eval.max_templates
-  }
-  for k, v in feat.items():
-    # Don't transfer this to the accelerator.
-    if k == 'extra_cluster_assignment':
-      continue
-    shape = list(v.shape)
-    schema = shape_schema[k]
-    assert len(shape) == len(schema), (
-        f'Rank mismatch between shape and shape schema for {k}: '
-        f'{shape} vs {schema}')
-    pad_size = [pad_size_map.get(s2, None) or s1 for (s1, s2) in zip(shape, schema)]
-    padding = [(0, p - tf.shape(v)[i]) for i, p in enumerate(pad_size)]
-    if padding:
-      feat[k] = tf.pad(v, padding, name=f'pad_to_fixed_{k}')
-      feat[k].set_shape(pad_size)
-  return {k:np.asarray(v) for k,v in feat.items()}
+from alphafold.common import protein, residue_constants
+from alphafold.model import model, folding, all_atom
 
 #########################
 # rmsd
@@ -460,7 +423,6 @@ def save_pdb(outs, filename="tmp.pdb"):
 
 order_restype = {v: k for k, v in residue_constants.restype_order.items()}
 idx_to_resname = dict((v,k) for k,v in residue_constants.resname_to_idx.items())
-template_aa_map = np.eye(20)[[residue_constants.HHBLITS_AA_TO_ID[order_restype[i]] for i in range(20)]].T
 
 ###########################
 # MISC
@@ -473,20 +435,3 @@ jalview_color_list = {"Clustal":           ["#80a0f0","#f01505","#00ff00","#c048
                       "Strand Propensity": ["#5858a7","#6b6b94","#64649b","#2121de","#9d9d62","#8c8c73","#0000ff","#4949b6","#60609f","#ecec13","#b2b24d","#4747b8","#82827d","#c2c23d","#2323dc","#4949b6","#9d9d62","#c0c03f","#d3d32c","#ffff00","#4343bc","#797986","#4747b8"],
                       "Turn Propensity":   ["#2cd3d3","#708f8f","#ff0000","#e81717","#a85757","#3fc0c0","#778888","#ff0000","#708f8f","#00ffff","#1ce3e3","#7e8181","#1ee1e1","#1ee1e1","#f60909","#e11e1e","#738c8c","#738c8c","#9d6262","#07f8f8","#f30c0c","#7c8383","#5ba4a4"],
                       "Buried Index":      ["#00a35c","#00fc03","#00eb14","#00eb14","#0000ff","#00f10e","#00f10e","#009d62","#00d52a","#0054ab","#007b84","#00ff00","#009768","#008778","#00e01f","#00d52a","#00db24","#00a857","#00e619","#005fa0","#00eb14","#00b649","#00f10e"]}
-
-
-###########################
-# to be deprecated functions
-###########################
-def set_dropout(model_config, dropout=0.0):
-  model_config.model.embeddings_and_evoformer.evoformer.msa_row_attention_with_pair_bias.dropout_rate = dropout
-  model_config.model.embeddings_and_evoformer.evoformer.triangle_attention_ending_node.dropout_rate = dropout
-  model_config.model.embeddings_and_evoformer.evoformer.triangle_attention_starting_node.dropout_rate = dropout
-  model_config.model.embeddings_and_evoformer.evoformer.triangle_multiplication_incoming.dropout_rate = dropout
-  model_config.model.embeddings_and_evoformer.evoformer.triangle_multiplication_outgoing.dropout_rate = dropout
-  model_config.model.embeddings_and_evoformer.template.template_pair_stack.triangle_attention_ending_node.dropout_rate = dropout
-  model_config.model.embeddings_and_evoformer.template.template_pair_stack.triangle_attention_starting_node.dropout_rate = dropout
-  model_config.model.embeddings_and_evoformer.template.template_pair_stack.triangle_multiplication_incoming.dropout_rate = dropout
-  model_config.model.embeddings_and_evoformer.template.template_pair_stack.triangle_multiplication_outgoing.dropout_rate = dropout
-  model_config.model.heads.structure_module.dropout = dropout
-  return model_config

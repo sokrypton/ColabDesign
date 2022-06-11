9091cfb3a6a332d6f6d378f0f369f9183e859942
Sergey O <so@fas.harvard.edu>
2022-06-10 23:55:12 -0400

    Update model.py

--- af/src/model.py
+++ af/src/model.py
@@ -24,6 +24,7 @@ class mk_design_model(_af_init, _af_loss, _af_design, _af_utils):
     if protocol == "binder": use_templates = True
 
     self.protocol = protocol
+    self.use_struct = use_struct
     
     if not use_struct:
       assert num_recycles == 0
@@ -31,7 +32,7 @@ class mk_design_model(_af_init, _af_loss, _af_design, _af_utils):
     self.args = {"num_seq":num_seq, "use_templates":use_templates,
                  "model_mode":model_mode, "model_parallel": model_parallel,
                  "recycle_mode":recycle_mode, "use_pssm":use_pssm, "debug":debug,
-                 "repeat": False,"use_struct":use_struct}
+                 "repeat": False}
     
     self._default_opt = {"temp":1.0, "soft":0.0, "hard":0.0,"gumbel":False,
                          "dropout":True, "dropout_scale":1.0,

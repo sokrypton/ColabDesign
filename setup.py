115fe9080f93a946a426f5131dc6c51857bf44c9
Sergey O <so@fas.harvard.edu>
2022-06-23 23:07:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 setup(
     name='ColabDesign',
     version='0.0.0',
-    packages=find_packages(include=['af','af.src']),
+    packages=find_packages(include=['af','af.src','tr','tr.src']),
     install_requires=['py3Dmol'],
 )

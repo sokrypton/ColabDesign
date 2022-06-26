bcd50c3f00768dd6b5b1b635655fccd893e5e49f
Sergey O <so@fas.harvard.edu>
2022-06-25 21:59:51 -0400

    rm model_parallel option
    might bring it back later...

--- af/src/model.py
+++ af/src/model.py
@@ -15,7 +15,7 @@ from af.src.design import _af_design
 
 class mk_design_model(_af_prep, _af_loss, _af_design, _af_utils):
   def __init__(self, protocol="fixbb", num_seq=1,
-               num_models=1, model_sample=True, model_parallel=False,
+               num_models=1, model_sample=True,
                recycle_mode="average", num_recycles=0,
                use_templates=False, use_pssm=False, data_dir=".",
                debug=False, use_struct=True):
@@ -27,7 +27,7 @@ class mk_design_model(_af_prep, _af_loss, _af_design, _af_utils):
     self.use_struct = use_struct
     self.args = {"num_seq":num_seq, "use_templates":use_templates,
                  "recycle_mode": recycle_mode,
-                 "model_sample":model_sample, "model_parallel": model_parallel,
+                 "model_sample":model_sample,
                  "use_pssm":use_pssm, "debug":debug,
                  "repeat": False}
     
@@ -92,13 +92,7 @@ class mk_design_model(_af_prep, _af_loss, _af_design, _af_utils):
       self._model_params.append({k: params[k] for k in self._runner.params.keys()})
 
     # define gradient function
-    if self.args["model_parallel"]:
-      self._sample_params = jax.jit(lambda y,n:jax.tree_map(lambda x:x[n],y))
-      in_axes = (None,0,None,None,None)
-      self._model_params = jax.tree_util.tree_map(lambda *x:jnp.stack(x),*self._model_params)
-      self._grad_fn, self._fn = [jax.jit(jax.vmap(x,in_axes)) for x in self._get_fn()]
-    else:        
-      self._grad_fn, self._fn = [jax.jit(x) for x in self._get_fn()]
+    self._grad_fn, self._fn = [jax.jit(x) for x in self._get_fn()]
 
     # define input function
     if protocol == "fixbb":           self.prep_inputs = self._prep_fixbb

2ae2f984b82351ebbe84e5b02796677a8e8b2891
Sergey O <so@fas.harvard.edu>
2022-06-26 01:58:04 -0400

    updating update_dict to be more robust

--- tr/src/utils.py
+++ tr/src/utils.py
@@ -1,8 +1,28 @@
-def update_dict(d, u=None):
-  if u is not None:
-    for k, v in u.items():
-      if v is not None:
-        if isinstance(v, dict):
-          update_dict(d.get(k,{}), v)
+import jax
+
+def update_dict(D, *args, **kwargs):
+  def set_zero(x):
+    for k,v in x.items():
+      if isinstance(v,dict): set_zero(v)
+      else: x[k] = type(x[k])(False)
+  
+  def set_dict(d, x):
+    for k,v in x.items():
+      if k in d:
+        if isinstance(v,dict):
+          set_dict(d[k], x[k])
+        elif isinstance(d[k],dict):
+          print(f"ERROR: '{k}' is a dictionary")
         else:
-          d.update({k:v})
+          d[k] = type(d[k])(v)
+      else:
+        print(f"ERROR: '{k}' not found in {list(d.keys())}")
+  
+  if "zero_all" in kwargs and kwargs["zero_all"]:
+    set_zero(D)
+    kwargs.pop("zero_all")
+  
+  for a in args:
+    if isinstance(a, dict): set_dict(D, a)    
+  
+  set_dict(D, kwargs)

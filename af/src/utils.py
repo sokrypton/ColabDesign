86b447c3a6768fbac7cc3e11a677a4d0b32a9e13
Sergey O <so@fas.harvard.edu>
2022-06-26 12:02:42 -0400

    Update utils.py

--- af/src/utils.py
+++ af/src/utils.py
@@ -28,6 +28,26 @@ pymol_color_list = ["#33ff33","#00ffff","#ff33cc","#ffff00","#ff9999","#e5e5e5",
                     "#339933","#66b2b2","#ba8c84","#84bf00","#b24c66","#7f7f7f","#3f3fa5","#a5512b"]
 pymol_cmap = matplotlib.colors.ListedColormap(pymol_color_list)
 
+# robust function for updating dictionary
+def update_dict(D, *args, **kwargs):
+  def set_dict(d, x):
+    for k,v in x.items():
+      if v is not None:
+        if k in d:
+          if isinstance(v,dict):
+            set_dict(d[k], x[k])
+          elif isinstance(d[k],dict):
+            print(f"ERROR: '{k}' is a dictionary")
+          elif isinstance(d[k],(int,float,bool,str)):
+            d[k] = type(d[k])(v)
+          else:
+            d[k] = v
+        else:
+          print(f"ERROR: '{k}' not found in {list(d.keys())}")  
+  for a in args:
+    if isinstance(a, dict): set_dict(D, a)    
+  set_dict(D, kwargs)
+
 ####################################################
 # AF_UTILS - various utils (save, plot, etc)
 ####################################################

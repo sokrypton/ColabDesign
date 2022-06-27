a0ffa2a93f39fea2ffdbd004f98ef5f6cc585760
Sergey O <so@fas.harvard.edu>
2022-06-26 23:21:49 -0400

    adding support for partial

--- tr/src/model.py
+++ tr/src/model.py
@@ -15,15 +15,15 @@ ORDER_RESTYPE = {v: k for k, v in residue_constants.restype_order.items()}
 class mk_trdesign_model():
   def __init__(self, protocol="fixbb", model_num=1, model_sample=True, data_dir="."):
     
-    assert protocol in ["fixbb","hallucination"]
+    assert protocol in ["fixbb","hallucination","partial"]
 
     self.protocol = protocol
     self._data_dir = data_dir
 
     # set default options
     self.opt = {"temp":1.0, "soft":1.0, "hard":1.0,
-                "weights":{},
-                "model":{"num":model_num,"sample":model_sample}}
+                "model":{"num":model_num,"sample":model_sample},
+                "weights":{}}
                 
     self.params = {"seq":None}
 
@@ -32,7 +32,7 @@ class mk_trdesign_model():
     self._model_params = [get_model_params(f"{data_dir}/models/model_xa{k}.npy") for k in list("abcde")]
     self._grad_fn, self._fn = [jax.jit(x) for x in self._get_model()]
 
-    if protocol in ["hallucination"]:
+    if protocol in ["hallucination","partial"]:
       self.bkg_model = TrRosetta(bkg_model=True)
   
   def _get_model(self):
@@ -50,31 +50,35 @@ class mk_trdesign_model():
       seq["pseudo"] = opt["soft"] * seq["soft"] + (1-opt["soft"]) * seq["input"]
       seq["pseudo"] = opt["hard"] * seq["hard"] + (1-opt["hard"]) * seq["pseudo"]
 
-      #if self.protocol in ["partial"]:
-      #  p = opt["pos"]
-      #  seq_ref = jax.nn.one_hot(self._wt_aatype,20)
-      #  seq = jax.tree_map(lambda x:x.at[:,p,:].set(seq_ref), seq)
-
+      if self.protocol in ["partial"] and "pos" in opt:
+        pos = opt["pos"]
+        seq_ref = jax.nn.one_hot(self._batch["aatype"],20)
+        seq = jax.tree_map(lambda x:x.at[pos,:].set(seq_ref), seq)
       return seq
     
     def _get_loss(outputs, opt):
       aux = {"outputs":outputs, "losses":{}}
-
-      log_p = jax.tree_map(jax.nn.log_softmax, outputs)            
-      # cce loss
-      if self.protocol in ["fixbb"]:
-        q = self.feats
-        aux["losses"]["cce"] = {}
-        for k in ["dist","omega","theta","phi"]:
-          aux["losses"]["cce"][k] = -(q[k]*log_p[k]).sum(-1).mean()
+      log_p = jax.tree_map(jax.nn.log_softmax, outputs)
 
       # bkg loss
-      if self.protocol in ["hallucination"]:
+      if self.protocol in ["hallucination","partial"]:
         p = jax.tree_map(jax.nn.softmax, outputs)
         log_q = jax.tree_map(jax.nn.log_softmax, self.bkg_feats)
         aux["losses"]["bkg"] = {}
         for k in ["dist","omega","theta","phi"]:
           aux["losses"]["bkg"][k] = -(p[k]*(log_p[k]-log_q[k])).sum(-1).mean()
+
+      # cce loss
+      if self.protocol in ["fixbb","partial"]:
+        if self.protocol in ["partial"] and "pos" in opt:
+          pos = opt["pos"]
+          log_p = jax.tree_map(lambda x:x[:,pos][pos,:], log_p)
+
+        q = self.feats
+        aux["losses"]["cce"] = {}
+        for k in ["dist","omega","theta","phi"]:
+          aux["losses"]["cce"][k] = -(q[k]*log_p[k]).sum(-1).mean()
+          print(k,log_p[k].shape)
 
       # weighted loss
       weighted_losses = jax.tree_map(lambda l, w: l * w, aux["losses"],opt["weights"])
@@ -90,15 +94,25 @@ class mk_trdesign_model():
 
     return jax.value_and_grad(_model, has_aux=True, argnums=0), _model
   
-  def prep_inputs(self, pdb_filename=None, chain=None, length=None, pos=None, **kwargs):    
+  def prep_inputs(self, pdb_filename=None, chain=None, length=None, pos=None,
+                  **kwargs):
+    
     if self.protocol in ["fixbb", "partial"]:
       # parse PDB file and return features compatible with TrRosetta
-      pdb = prep_pdb(pdb_filename, chain, for_alphafold=False)      
-      self.feats = _np_get_6D_binned(pdb["batch"]["all_atom_positions"],
-                                     pdb["batch"]["all_atom_mask"])      
-      self._wt_seq = pdb["batch"]["aatype"]
-      self._len = len(self._wt_seq)
-      self.opt["weights"]["cce"] = {"dist":0.25,"omega":0.25,"theta":0.25,"phi":0.25}
+      pdb = prep_pdb(pdb_filename, chain, for_alphafold=False)
+      self._batch = pdb["batch"]
+
+      if self.protocol == "partial":
+        if pos is not None:
+          p = prep_pos(pos, **pdb["idx"])
+          self._batch = jax.tree_map(lambda x:x[p], self._batch)
+          self.opt["pos"] = np.arange(len(p))
+
+      self.feats = _np_get_6D_binned(self._batch["all_atom_positions"],
+                                     self._batch["all_atom_mask"])
+
+      self._len = len(self._batch["aatype"])
+      self.opt["weights"]["cce"] = {"dist":1/6,"omega":1/6,"theta":2/6,"phi":2/6}
 
     if self.protocol in ["hallucination", "partial"]:
       # compute background distribution
@@ -109,7 +123,7 @@ class mk_trdesign_model():
         model_params = get_model_params(f"{self._data_dir}/bkgr_models/bkgr0{n}.npy")
         self.bkg_feats.append(self.bkg_model(model_params, key, self._len))
       self.bkg_feats = jax.tree_map(lambda *x:jnp.stack(x).mean(0), *self.bkg_feats)
-      self.opt["weights"]["bkg"] = {"dist":0.25,"omega":0.25,"theta":0.25,"phi":0.25}
+      self.opt["weights"]["bkg"] = {"dist":1/6,"omega":1/6,"theta":2/6,"phi":2/6}
 
     self.restart(**kwargs)
   
@@ -207,11 +221,11 @@ def _np_get_6D_binned(all_atom_positions, all_atom_mask):
   def mtx2bins(x_ref, start, end, nbins, mask):
     bins = np.linspace(start, end, nbins)
     x_true = np.digitize(x_ref, bins).astype(np.uint8)
-    x_true[mask] = 0
+    x_true = np.where(mask,0,x_true)
     return np.eye(nbins+1)[x_true][...,:-1]
 
   mask = (ref["dist"] > 20) | (np.eye(ref["dist"].shape[0]) == 1)
-  return {"dist":mtx2bins(ref["dist"],     2.0,  20.0,  37,  mask=mask),
+  return {"dist": mtx2bins(ref["dist"],    2.0,  20.0,  37,  mask=mask),
           "omega":mtx2bins(ref["omega"], -np.pi, np.pi, 25,  mask=mask),
           "theta":mtx2bins(ref["theta"], -np.pi, np.pi, 25,  mask=mask),
           "phi":  mtx2bins(ref["phi"],      0.0, np.pi, 13,  mask=mask)}

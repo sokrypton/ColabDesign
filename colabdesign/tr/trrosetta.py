3690d51e7ce0a56e4e7dcb7f4f6471e3c02c2cd5
Sergey O <so@fas.harvard.edu>
2022-06-28 07:46:18 -0400

    adding design code to trrosetta

--- colabdesign/tr/trrosetta.py
+++ colabdesign/tr/trrosetta.py
@@ -1,101 +1,316 @@
-import jax.numpy as jnp
-import jax
+import random, os
 import numpy as np
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
 
-def TrRosetta(bkg_model=False):  
-  
-  def pseudo_mrf(seq):
-    '''single sequence'''    
-    L,A = seq.shape[0],21
-    prf = jnp.pad(seq,[[0,0],[0,1]])
+from colabdesign.tr.utils import update_dict
+from colabdesign.tr.trrosetta import TrRosetta, get_model_params
+
+# borrow some stuff from AfDesign
+from colabdesign.af.misc import _np_get_6D
+from colabdesign.af.prep import prep_pdb, prep_pos
+from colabdesign.af.alphafold.common import protein, residue_constants
+ORDER_RESTYPE = {v: k for k, v in residue_constants.restype_order.items()}
+
+try:
+  from jax.example_libraries.optimizers import sgd, adam
+except:
+  from jax.experimental.optimizers import sgd, adam
+
+class mk_trdesign_model():
+  def __init__(self, protocol="fixbb", model_num=1, model_sample=True, data_dir="params/tr"):
     
-    # 1D features
-    x_1D = jnp.concatenate([seq, prf],-1)
-    x_1D = jnp.pad(x_1D,[[0,0],[0,1]])
-    x_1D = jnp.repeat(x_1D[None],L,0)
+    assert protocol in ["fixbb","hallucination","partial"]
 
-    # 2D features
-    x_2D = jnp.diag(jnp.full(L*A,0.4))
-    x_2D = x_2D.reshape(L,A,L,A).swapaxes(1,2).reshape(L,L,-1)
-    x_2D = jnp.pad(x_2D,[[0,0],[0,0],[0,1]])
-    return jnp.concatenate([x_1D.swapaxes(0,1), x_1D, x_2D],-1)
+    self.protocol = protocol
+    self._data_dir = "." if os.path.isfile("models/model_xaa.npy") else data_dir
+
+    # set default options
+    self._opt = {"temp":1.0, "soft":1.0, "hard":1.0,
+                 "model":{"num":model_num,"sample":model_sample},
+                 "weights":{}, "lr":1.0}
+                
+    self.params = {"seq":None}
+
+    # setup model
+    self._runner = TrRosetta()
+    self._model_params = [get_model_params(f"{self._data_dir}/models/model_xa{k}.npy") for k in list("abcde")]
+    self._grad_fn, self._fn = [jax.jit(x) for x in self._get_model()]
+
+    if protocol in ["hallucination","partial"]:
+      self.bkg_model = TrRosetta(bkg_model=True)
   
-  def instance_norm(x, p):
-    mu = x.mean((0,1),keepdims=True)
-    var = x.var((0,1),keepdims=True)
-    inv = jax.lax.rsqrt(var + 1e-6) * p ["scale"]
-    return x * inv + p["offset"] - mu * inv
+  def _get_model(self):
+    def _get_seq(params, opt):
+      seq = {"input":params["seq"]}
 
-  def conv_2D(x, p, dilation=1, stride=1, padding="SAME"):
-    flags = dict(window_strides=(stride,stride),
-                 rhs_dilation=(dilation,dilation),
-                 padding=padding)
-    x = x.transpose([2,0,1])
-    f = p["filters"].transpose([3,2,0,1])
-    x = jax.lax.conv_general_dilated(x[None], f, **flags)[0]
-    x = x.transpose([1,2,0])
-    return x + p["bias"]
+      # straight-through/reparameterization
+      seq["logits"] = seq["input"]
+      seq["soft"] = jax.nn.softmax(seq["logits"] / opt["temp"])
+      seq["hard"] = jax.nn.one_hot(seq["soft"].argmax(-1), 20)
+      seq["hard"] = jax.lax.stop_gradient(seq["hard"] - seq["soft"]) + seq["soft"]
 
-  def dense(x, p):
-    return x @ p["filters"] + p["bias"]
+      # create pseudo sequence
+      seq["pseudo"] = opt["soft"] * seq["soft"] + (1-opt["soft"]) * seq["input"]
+      seq["pseudo"] = opt["hard"] * seq["hard"] + (1-opt["hard"]) * seq["pseudo"]
+
+      if self.protocol in ["partial"] and "pos" in opt:
+        pos = opt["pos"]
+        seq_ref = jax.nn.one_hot(self._batch["aatype"],20)
+        seq = jax.tree_map(lambda x: jnp.where(opt["fix_seq"],x.at[pos,:].set(seq_ref),x), seq)
+      return seq
+    
+    def _get_loss(outputs, opt):
+      aux = {"outputs":outputs, "losses":{}}
+      log_p = jax.tree_map(jax.nn.log_softmax, outputs)
+
+      # bkg loss
+      if self.protocol in ["hallucination","partial"]:
+        p = jax.tree_map(jax.nn.softmax, outputs)
+        log_q = jax.tree_map(jax.nn.log_softmax, self._bkg_feats)
+        aux["losses"]["bkg"] = {}
+        for k in ["dist","omega","theta","phi"]:
+          aux["losses"]["bkg"][k] = -(p[k]*(log_p[k]-log_q[k])).sum(-1).mean()
+
+      # cce loss
+      if self.protocol in ["fixbb","partial"]:
+        if self.protocol in ["partial"] and "pos" in opt:
+          pos = opt["pos"]
+          log_p = jax.tree_map(lambda x:x[:,pos][pos,:], log_p)
+
+        q = self._feats
+        aux["losses"]["cce"] = {}
+        for k in ["dist","omega","theta","phi"]:
+          aux["losses"]["cce"][k] = -(q[k]*log_p[k]).sum(-1).mean()
+
+      # weighted loss
+      weighted_losses = jax.tree_map(lambda l, w: l * w, aux["losses"],opt["weights"])
+      loss = jax.tree_leaves(weighted_losses)
+      return sum(loss), aux
+
+    def _model(params, model_params, opt):
+      seq = _get_seq(params, opt)
+      outputs = self._runner(seq["pseudo"], model_params)
+
+      loss, aux = _get_loss(outputs, opt)
+      return loss, aux
+
+    return jax.value_and_grad(_model, has_aux=True, argnums=0), _model
   
-  def encoder(x, p):
-    x = dense(x,p)
-    x = instance_norm(x,p)
-    return jax.nn.elu(x)
+  def prep_inputs(self, pdb_filename=None, chain=None, length=None,
+                  pos=None, fix_seq=False, **kwargs):
+    '''
+    prep inputs for TrDesign
+    '''    
+    if self.protocol in ["fixbb", "partial"]:
+      # parse PDB file and return features compatible with TrRosetta
+      pdb = prep_pdb(pdb_filename, chain, for_alphafold=False)
+      self._batch = pdb["batch"]
+
+      if self.protocol in ["partial"] and pos is not None:
+        p = prep_pos(pos, **pdb["idx"])
+        self._batch = jax.tree_map(lambda x:x[p], self._batch)
+        self._opt["pos"] = np.arange(len(p))
+        self._opt["fix_seq"] = fix_seq
+
+      self._feats = _np_get_6D_binned(self._batch["all_atom_positions"],
+                                      self._batch["all_atom_mask"])
+
+      self._len = len(self._batch["aatype"])
+      self._opt["weights"]["cce"] = {"dist":1/6,"omega":1/6,"theta":2/6,"phi":2/6}
+
+    if self.protocol in ["hallucination", "partial"]:
+      # compute background distribution
+      if length is not None: self._len = length
+      self._bkg_feats = []
+      key = jax.random.PRNGKey(0)
+      for n in range(1,6):
+        model_params = get_model_params(f"{self._data_dir}/bkgr_models/bkgr0{n}.npy")
+        self._bkg_feats.append(self.bkg_model(model_params, key, self._len))
+      self._bkg_feats = jax.tree_map(lambda *x:jnp.stack(x).mean(0), *self._bkg_feats)
+      self._opt["weights"]["bkg"] = {"dist":1/6,"omega":1/6,"theta":2/6,"phi":2/6}
+
+    self.restart(**kwargs)
+
+  def set_opt(self, *args, **kwargs):
+    update_dict(self.opt, *args, **kwargs)
+
+  def set_weights(self, *args, **kwargs):
+    update_dict(self.opt["weights"], *args, **kwargs)
+
+  def set_seq(self, seq):
+    if isinstance(seq, str):
+      seq = np.array([residue_constants.restype_order.get(aa,-1) for aa in seq])
+      seq = np.eye(20)[seq]
+    assert seq.shape[-2] == self._len
+    update_dict(self.params, {"seq":seq})
+  
+  def _init_seq(self, seq=None):
+    if seq is None:
+      self._key, key = jax.random.split(self._key)
+      seq = 0.01 * jax.random.normal(key, (self._len,20))
+    self.set_seq(seq)
+
+  def _init_optimizer(self):
+    '''setup which optimizer to use'''      
+    self._init_fun, self._update_fun, self._get_params = sgd(1.0)
+    self._k = 0
+    self._state = self._init_fun(self.params)
+
+  def restart(self, seed=None, opt=None, weights=None, seq=None):
+    self._seed = random.randint(0,2147483647) if seed is None else seed
+    self._key = jax.random.PRNGKey(self._seed)
+    self.opt = jax.tree_map(lambda x:x, self._opt) # copy
+    self.set_opt(opt)
+    self.set_weights(weights)
+    self._init_seq(seq)
+    self._init_optimizer()
+    
+  def run(self, backprop=True):
+    '''run model to get outputs, losses and gradients'''
+    
+    # decide which model params to use
+    m = self.opt["model"]["num"]
+    ns = jnp.arange(5)
+    if self.opt["model"]["sample"] and m != len(ns):
+      self._key, key = jax.random.split(self._key)
+      model_num = jax.random.choice(key,ns,(m,),replace=False)
+    else:
+      model_num = ns[:m]
+    model_num = np.array(model_num).tolist()
+
+    # run in serial
+    _loss, _aux, _grad = [],[],[]
+    for n in model_num:
+      model_params = self._model_params[n]
+      if backprop:
+        (l,a),g = self._grad_fn(self.params, model_params, self.opt)
+        _grad.append(g)
+      else:
+        l,a = self._fn(self.params, model_params, self.opt)
+      _loss.append(l)
+      _aux.append(a)
+    
+    # average results
+    if len(model_num) > 1:
+      _loss = jnp.asarray(_loss).mean()
+      _aux = jax.tree_map(lambda *v: jnp.stack(v).mean(0), *_aux)
+      if backprop: _grad = jax.tree_map(lambda *v: jnp.stack(v).mean(0), *_grad)
+    else:
+      _loss,_aux = _loss[0],_aux[0]
+      if backprop: _grad = _grad[0] 
+
+    if not backprop:
+      _grad = jax.tree_map(jnp.zeros_like, self.params)
+
+    # update
+    self.loss = _loss
+    self.aux = _aux
+    self.aux["model_num"] = model_num
+    self.grad = _grad
+
+  def step(self, backprop=True, callback=None, verbose=True):
+    self.run(backprop=backprop)
+    if callback is not None: callback(self)
+
+    # normalize gradient
+    g = self.grad["seq"]
+    gn = jnp.linalg.norm(g,axis=(-1,-2),keepdims=True)
+    self.grad["seq"] *= jnp.sqrt(self._len)/(gn+1e-7)
+
+    # set learning rate
+    self.grad = jax.tree_map(lambda x:x*self.opt["lr"], self.grad)
+
+    # apply gradient
+    self._state = self._update_fun(self._k, self.grad, self._state)
+    self.params = self._get_params(self._state)    
+    self._k += 1
+
+    if verbose:
+      print(self._k, self.aux["model_num"], self.get_loss())
+
+  def design(self, iters=100, opt=None, weights=None, **kwargs):
+    self.set_opt(opt)
+    self.set_weights(weights)
+    for _ in range(iters):
+      self.step(**kwargs)
+
+  def _plot(self, x, dpi=100):
+    x = jax.tree_map(np.asarray, x)
+    plt.figure(figsize=(4*4,4), dpi=dpi)
+    for n,k in enumerate(["theta","phi","dist","omega"]):
+      v = x[k]
+      plt.subplot(1,4,n+1)
+      plt.title(k)
+      plt.imshow(v.argmax(-1),cmap="binary")
+    plt.show()
+
+  def predict(self, seq=None):
+    '''make prediction'''
+    if seq is not None: self.set_seq(seq)
+    self.run(backprop=False)
+  
+  def plot(self, mode="preds", dpi=100):
+    '''plot predictions'''
+    assert mode in ["preds","feats","bkg_feats"]
+    if mode == "preds": x = self.aux["outputs"]
+    if mode == "feats": x = self._feats
+    if mode == "bkg_feats": x = self._bkg_feats
+    self._plot(x, dpi=dpi)
+    
+  def get_loss(self, k=None):
+    if k is None:
+      return {k:self.get_loss(k) for k in self.aux["losses"].keys()}
+    losses = self.aux["losses"][k]
+    weights = self.opt["weights"][k]
+    weighted_losses = jax.tree_map(lambda l,w:l*w, losses, weights)
+    return float(sum(jax.tree_leaves(weighted_losses)))
+    
+  def af_callback(self, weight=1.0, add_loss=True):    
+    def callback(af_model):
       
-  def block(x, p, dilation=1):
-    y = x
-    for n in [0,1]:
-      q = jax.tree_map(lambda x:x[n],p)
-      y = conv_2D(y,q,dilation)
-      y = instance_norm(y,q)
-      y = jax.nn.elu(y if n == 0 else (x+y))
-    return y  
-  
-  def resnet(x, p):
-    def body(y, p_sub):
-      for n,dilation in enumerate([1,2,4,8,16]):
-        q = jax.tree_map(lambda x:x[n],p_sub)
-        y = block(y,q,dilation)
-      return y, None
-    return jax.lax.scan(body,x,p)[0]
-  
-  def heads(x, p):
-    o = {k:dense(x,p[k]) for k in ["theta","phi"]}
-    x = (x + x.swapaxes(0,1)) / 2  
-    o.update({k:dense(x,p[k]) for k in ["dist","bb","omega"]})
-    return o    
-  
-  def trunk(x,p):
-    x = encoder(x, p["encoder"])
-    x = resnet(x, p["resnet"])
-    x = block(x, p["block"])  
-    return heads(x, p)
-  
-  # decide which model to use
-  if bkg_model:
-    def model(model_params, seed, length=100):
-      x = jax.random.normal(seed, (length, length, 64))
-      return trunk(x,model_params)
-    return jax.jit(model, static_argnums=2)
-  else:
-    def model(seq, model_params):
-      x = pseudo_mrf(seq)
-      return trunk(x,model_params)
-    return jax.jit(model)
+      # copy [opt]ions from afdesign
+      for k in ["soft","temp","hard","pos"]:
+        if k in self.opt and k in af_model.opt:
+          self.opt[k] = af_model.opt[k]
 
-def get_model_params(npy):
-  '''parse TrRosetta params into dictionary'''
-  xaa = np.load(npy,allow_pickle=True).tolist()
-  layers = ["encoder","resnet","block","theta","phi","dist","bb","omega"]
-  num = np.array([4,0,8,2,2,2,2,2])
-  num[1] = len(xaa) - num.sum()
-  idx = np.cumsum(num) - num
-  def split(params):
-    labels = ["filters","bias","offset","scale"]
-    steps = min(len(params),len(labels))
-    return {labels[n]:np.squeeze(params[n::steps]) for n in range(steps)}
-  params = {k:split(xaa[i:i+n]) for k,i,n in zip(layers,idx,num)}
-  params["resnet"] = jax.tree_map(lambda x:x.reshape(-1,5,2,*x.shape[1:]), params["resnet"])
-  return params 
+      # update sequence input
+      self.params["seq"] = af_model.params["seq"][0]
+      
+      # run trdesign
+      self.run(backprop = weight > 0)
+      
+      # add gradients
+      af_model.grad["seq"] += weight * self.grad["seq"]
+      
+      # add loss
+      if add_loss:
+        af_model.loss += weight * self.loss
+        
+      # for verbose printout
+      if self.protocol in ["hallucination","partial"]:
+        af_model.aux["losses"]["TrD_bkg"] = self.get_loss("bkg")
+      if self.protocol in ["fixbb","partial"]:
+        af_model.aux["losses"]["TrD_cce"] = self.get_loss("cce")
+      
+    return callback
+
+def _np_get_6D_binned(all_atom_positions, all_atom_mask):
+  # TODO: make differentiable, add use_jax option
+  ref = _np_get_6D(all_atom_positions,
+                   all_atom_mask,
+                   use_jax=False, for_trrosetta=True)
+  ref = jax.tree_map(jnp.squeeze,ref)
+
+  def mtx2bins(x_ref, start, end, nbins, mask):
+    bins = np.linspace(start, end, nbins)
+    x_true = np.digitize(x_ref, bins).astype(np.uint8)
+    x_true = np.where(mask,0,x_true)
+    return np.eye(nbins+1)[x_true][...,:-1]
+
+  mask = (ref["dist"] > 20) | (np.eye(ref["dist"].shape[0]) == 1)
+  return {"dist": mtx2bins(ref["dist"],    2.0,  20.0,  37,  mask=mask),
+          "omega":mtx2bins(ref["omega"], -np.pi, np.pi, 25,  mask=mask),
+          "theta":mtx2bins(ref["theta"], -np.pi, np.pi, 25,  mask=mask),
+          "phi":  mtx2bins(ref["phi"],      0.0, np.pi, 13,  mask=mask)}

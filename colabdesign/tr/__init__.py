8b7ea7d982d6202687cc93bcc2927b64a27552cd
Sergey O <so@fas.harvard.edu>
2022-06-30 13:57:57 -0400

    typos

--- colabdesign/tr/__init__.py
+++ colabdesign/tr/__init__.py
@@ -1,5 +1,5 @@
 from colabdesign.tr.model import mk_trdesign_model
-from colabdesign.utils import clear_mem
+from colabdesign.shared.utils import clear_mem
 
 # backward compatability
 mk_design_model = mk_trdesign_model

b95408c976bb5397855db8dc82326dd980587251
Sergey O <so@fas.harvard.edu>
2022-08-21 15:17:44 -0400

    change order of verbose print

--- colabdesign/af/design.py
+++ colabdesign/af/design.py
@@ -220,7 +220,7 @@ class _af_design:
   def _print_log(self, print_str=None):
     keys = ["models","recycles","hard","soft","temp","seqid","loss",
             "msa_ent","plddt","pae","helix","con","i_pae","i_con",
-            "sc_fape","sc_rmsd","dgram_cce","fape","ptm"]
+            "sc_fape","sc_rmsd","dgram_cce","i_fape","fape","ptm"]
     if sum(self._lengths) > 0: keys.append("i_ptm")
     keys.append("rmsd")
     print(dict_to_str(self.aux["log"], filt=self.opt["weights"],

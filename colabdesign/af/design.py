bd9e893e4389a17d3b552561823809f1e4c35402
Sergey O <so@fas.harvard.edu>
2022-09-09 01:06:51 -0400

    revert

--- colabdesign/af/design.py
+++ colabdesign/af/design.py
@@ -151,7 +151,6 @@ class _af_design:
 
     if mode in ["backprop","add_prev"]:
       # recycles compiled into model, only need single-pass
-      num_recycles = self.opt["num_recycles"] = self._cfg.model.num_recycle
       aux = self._single(model_params, backprop)
     
     else:

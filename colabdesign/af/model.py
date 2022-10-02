b508a55185d3f8cdc38218c3ac4036f983e35458
Sergey O <so@fas.harvard.edu>
2022-10-02 11:05:04 -0400

    cleanup

--- colabdesign/af/model.py
+++ colabdesign/af/model.py
@@ -51,7 +51,7 @@ class mk_af_model(design_model, _af_inputs, _af_loss, _af_prep, _af_design, _af_
                 "temp":1.0, "soft":0.0, "hard":0.0, "bias":0.0, "alpha":2.0,
                 "con":      {"num":2, "cutoff":14.0, "binary":False, "seqsep":9, "num_pos":float("inf")},
                 "i_con":    {"num":1, "cutoff":21.6875, "binary":False, "num_pos":float("inf")},
-                "template": {"dropout":0.0, "rm_ic":False, "rm_seq":True, "rm_sc":True},                
+                "template": {"dropout":0.0, "rm_ic":False},                
                 "weights":  {"seq_ent":0.0, "plddt":0.0, "pae":0.0, "exp_res":0.0},
                 "cmap_cutoff": 10.0, "fape_cutoff":10.0}
 

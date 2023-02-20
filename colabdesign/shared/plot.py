243fcb794b3f3dfa750a748340001c57b821c180
Shenjun Zhong <zhongsj52@gmail.com>
2023-02-20 22:46:49 +1100

    Update plot.py
    replace np.float alias with float for numpy version greater than 1.24

--- colabdesign/shared/plot.py
+++ colabdesign/shared/plot.py
@@ -108,7 +108,7 @@ def plot_pseudo_3D(xyz, c=None, ax=None, chainbreak=5,
   
   if chainbreak is not None:
     dist = np.linalg.norm(xyz[:-1] - xyz[1:], axis=-1)
-    colors[...,3] = (dist < chainbreak).astype(np.float)
+    colors[...,3] = (dist < chainbreak).astype(float)
 
   # add shade/tint based on z-dimension
   z = rescale(seg_z,zmin,zmax)[:,None]

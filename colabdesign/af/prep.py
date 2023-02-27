6b12f72f1c2ad96306cc6807f3f69f1964268c8b
Sergey O <so@fas.harvard.edu>
2023-02-27 06:10:31 -0500

    add cyclic support

--- colabdesign/af/prep.py
+++ colabdesign/af/prep.py
@@ -357,6 +357,28 @@ class _af_prep:
   
     self._prep_model(**kwargs)
 
+  def set_cyclic_offset(self):
+    '''add cyclic offset'''
+    def cyclic_offset(L):
+        i = np.arange(L)
+        ij = np.stack([i,i+L],-1)
+        offset = i[:,None] - i[None,:]
+        c_offset = np.abs(ij[:,None,:,None] - ij[None,:,None,:]).min((2,3))
+        return np.sign(offset) * c_offset
+    idx = self._inputs["residue_index"]
+    offset = np.array(idx[:,None] - idx[None,:])
+
+    if self.protocol == "binder":
+      c_offset = cyclic_offset(self._binder_len)
+      offset[self._target_len:,self._target_len:] = c_offset
+
+    if self.protocol in ["fixbb","partial","hallucination"]:
+      Ln = 0
+      for L in self._lengths:
+        offset[Ln:Ln+L,Ln:Ln+L] = cyclic_offset(L)
+        Ln += L
+    self._inputs["offset"] = offset
+
 #######################
 # utils
 #######################

dd6dce84b0f4d163336b848eb8d49fea9c9c67f9
Sergey O <so@fas.harvard.edu>
2023-04-15 08:40:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 setup(
     name='colabdesign',
     version='1.1.1',
+    description='Making Protein Design accessible to all via Google Colab!',
+    long_description="Making Protein Design accessible to all via Google Colab!",
+    long_description_content_type='text/markdown',
     packages=find_packages(include=['colabdesign*']),
     install_requires=['py3Dmol','absl-py','biopython',
                       'chex','dm-haiku','dm-tree',

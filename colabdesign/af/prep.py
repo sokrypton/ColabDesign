c8ca6ba7b2c29140943ec8e21e3d321c18d3c6cb
Sergey O <so@fas.harvard.edu>
2023-06-28 04:24:48 -0700

    Update prep.py

--- colabdesign/af/prep.py
+++ colabdesign/af/prep.py
@@ -167,20 +167,20 @@ class _af_prep:
     self._inputs["batch"] = batch
     
     # define masks
-    unsupervised = np.array(unsupervised).astype(float)
-    unsupervised_2d = np.logical_or(unsupervised[:,None],unsupervised[None,:]).astype(float)
+    unsupervised = np.array(unsupervised)
+    unsupervised_2d = np.logical_or(unsupervised[:,None], unsupervised[None,:])
 
-    fix_pos = np.full(num_res,kwargs.pop("fix_all_pos",False)).astype(float)
+    fix_pos = np.full(num_res,kwargs.pop("fix_all_pos",False))
     if "fix_pos" in pos_cst:
-      fix_pos[pos_cst.pop("fix_pos")] = 1
+      fix_pos[pos_cst.pop("fix_pos")] = True
 
     self._inputs.update({
-      "unsupervised":unsupervised,
-      "unsupervised_2d":unsupervised_2d,
-      "supervised":(unsupervised == False).astype(float),
-      "supervised_2d":(unsupervised_2d == False).astype(float),
-      "interchain_mask":interchain_mask,
-      "fix_pos":fix_pos
+      "unsupervised":unsupervised.astype(int),
+      "unsupervised_2d":unsupervised_2d.astype(int),
+      "supervised":(unsupervised == False).astype(int),
+      "supervised_2d":(unsupervised_2d == False).astype(int),
+      "interchain_mask":interchain_mask.astype(int),
+      "fix_pos":fix_pos.astype(int)
     })
     
     # set positional constraints

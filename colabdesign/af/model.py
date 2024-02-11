bacf5ce547113753d01cc0ca2846e3edebcde05b
Julia Varga <jvarga92@gmail.com>
2024-02-11 16:27:30 +0200

    returning dgram

--- colabdesign/af/model.py
+++ colabdesign/af/model.py
@@ -12,7 +12,7 @@ from colabdesign.shared.utils import Key
 
 from colabdesign.af.prep   import _af_prep
 from colabdesign.af.loss   import _af_loss, get_plddt, get_pae, get_ptm
-from colabdesign.af.loss   import get_contact_map
+from colabdesign.af.loss   import get_contact_map, get_dgram_bins
 from colabdesign.af.utils  import _af_utils
 from colabdesign.af.design import _af_design
 from colabdesign.af.inputs import _af_inputs
@@ -208,7 +208,10 @@ class mk_af_model(design_model, _af_inputs, _af_loss, _af_prep, _af_design, _af_
                   "plddt":          get_plddt(outputs),
                   "pae":            get_pae(outputs), 
                   "ptm":            get_ptm(inputs, outputs),
+                  "dgram":          outputs["distogram"]["logits"],
+                  "dgram_bins":     get_dgram_bins(outputs),
                   "i_ptm":          get_ptm(inputs, outputs, interface=True), 
+                  "ic_ptm":          get_ptm(inputs, outputs, interface=True),
                   "cmap":           get_contact_map(outputs, opt["con"]["cutoff"]),
                   "i_cmap":         get_contact_map(outputs, opt["i_con"]["cutoff"]),
                   "prev":           outputs["prev"]})

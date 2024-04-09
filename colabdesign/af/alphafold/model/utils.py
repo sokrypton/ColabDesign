0455f6e794fd4a52150478c5f7d63b062311b4f9
Julia Varga <jvarga92@gmail.com>
2024-04-10 02:10:01 +0300

    Revert "Revert "fixing jax.random.PRNGKeyArray error""
    This reverts commit fee5405da5a73079d4ba98824b988383f0f2591d.

--- colabdesign/af/alphafold/model/utils.py
+++ colabdesign/af/alphafold/model/utils.py
@@ -168,11 +168,9 @@ def padding_consistent_rng(f):
 
   def inner(key, shape, **kwargs):
     keys = grid_keys(key, shape)
-    signature = (
-        '()->()' if isinstance(keys, jax.random.PRNGKeyArray) else '(2)->()'
-    )
+    is_prng_key = jax.dtypes.issubdtype(keys.dtype, jax.dtypes.prng_key)
+    signature = '()->()' if is_prng_key else '(2)->()'
     return jnp.vectorize(
         functools.partial(f, shape=(), **kwargs), signature=signature
     )(keys)
-
   return inner

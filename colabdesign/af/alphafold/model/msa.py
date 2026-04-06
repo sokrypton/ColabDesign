5ab4efaba2321a6c3c314b82d2fff8e0241f5c2d
Sergey O <kingsergey@gmail.com>
2026-04-06 00:42:10 -0400

    Update msa.py

--- colabdesign/af/alphafold/model/msa.py
+++ colabdesign/af/alphafold/model/msa.py
@@ -88,7 +88,7 @@ def gumbel_argsort_sample_idx(key: jnp.ndarray,
   # This construction is equivalent to jnp.argsort, but using a non stable sort,
   # since stable sort's aren't supported by jax2tf.
   axis = len(logits.shape) - 1
-  iota = jax.lax.broadcasted_iota(jnp.int64, logits.shape, axis)
+  iota = jax.lax.broadcasted_iota(jnp.int32, logits.shape, axis)
   _, perm = jax.lax.sort_key_val(
       logits + z, iota, dimension=-1, is_stable=False)
   return perm[::-1]
